11028b4edbce04b52b2af0202d6e4b11f0d65d6b
FOZEU <fozeu.jm@gmail.com>
2023-08-01 20:53:17 +0200

    kafka producer configs

---
+++ app-config-data/src/main/java/com/kwd/config/KafkaProducerData.java
@@ -0,0 +1,90 @@
+package com.kwd.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "kafka-producer-config")
+public class KafkaProducerData {
+    private String keySerializerClass;
+    private String valueSerializerClass;
+    private String compressionType;
+    private String acks;
+    private Integer batchSize;
+    private Integer batchSizeBoostFactor;
+    private Integer lingerMs;
+    private Integer requestTimeoutMs;
+    private String retryCount;
+
+    public String getKeySerializerClass() {
+        return keySerializerClass;
+    }
+
+    public void setKeySerializerClass(String keySerializerClass) {
+        this.keySerializerClass = keySerializerClass;
+    }
+
+    public String getValueSerializerClass() {
+        return valueSerializerClass;
+    }
+
+    public void setValueSerializerClass(String valueSerializerClass) {
+        this.valueSerializerClass = valueSerializerClass;
+    }
+
+    public String getCompressionType() {
+        return compressionType;
+    }
+
+    public void setCompressionType(String compressionType) {
+        this.compressionType = compressionType;
+    }
+
+    public String getAcks() {
+        return acks;
+    }
+
+    public void setAcks(String acks) {
+        this.acks = acks;
+    }
+
+    public Integer getBatchSizeBoostFactor() {
+        return batchSizeBoostFactor;
+    }
+
+    public Integer getLingerMs() {
+        return lingerMs;
+    }
+
+    public void setLingerMs(Integer lingerMs) {
+        this.lingerMs = lingerMs;
+    }
+
+    public void setBatchSizeBoostFactor(Integer batchSizeBoostFactor) {
+        this.batchSizeBoostFactor = batchSizeBoostFactor;
+    }
+
+    public Integer getBatchSize() {
+        return batchSize;
+    }
+
+    public void setBatchSize(Integer batchSize) {
+        this.batchSize = batchSize;
+    }
+
+    public Integer getRequestTimeoutMs() {
+        return requestTimeoutMs;
+    }
+
+    public void setRequestTimeoutMs(Integer requestTimeoutMs) {
+        this.requestTimeoutMs = requestTimeoutMs;
+    }
+
+    public String getRetryCount() {
+        return retryCount;
+    }
+
+    public void setRetryCount(String retryCount) {
+        this.retryCount = retryCount;
+    }
+}
